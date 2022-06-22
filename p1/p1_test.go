6995d80431d3429fbfcb55956496e919a8e93791
Xiaoyi Shi <xshi@tencent.com>
2022-06-22 17:09:32 +0800

    update

--- p1/p1_test.go
+++ p1/p1_test.go
@@ -1,1 +1,10 @@
 package p1
+
+import "testing"
+
+func TestP1(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	NewMockP1(ctrl)
+	// ...
+}
--- p2/p2_test.go
+++ p2/p2_test.go
@@ -1,1 +1,10 @@
 package p2
+
+import "testing"
+
+func TestP1(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	NewMockP2(ctrl)
+	// ...
+}
